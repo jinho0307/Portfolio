a2e1b82a40bd85644416ca473a36dd4cbfd81dc1
jinho0307 <jinmon0307@gmail.com>
2017-08-29 18:53:05 +0900

    Update GroupWriteService.java

--- sins/src/main/java/com/seven/sins/group/service/GroupWriteService.java
+++ sins/src/main/java/com/seven/sins/group/service/GroupWriteService.java
@@ -6,12 +6,15 @@ import com.seven.sins.group.vo.GroupWriteVO;
 
 public interface GroupWriteService {
 	
+	// 해당 그룹 글 리스트 보여주기.
 	public ArrayList<GroupWriteVO> selectGroupWriteList(int groupNo);
 	
+	// 그룹에 글쓰기.
 	public int insertGroupWrite(GroupWriteVO vo);
 	
+	// 그룹에 쓴 글 수정.
 	public int updateGroupWrite(GroupWriteVO vo);
 	
+	// 그룹 글 삭제.
 	public int deleteGroupWrite(int writeNo);
-	
 }
